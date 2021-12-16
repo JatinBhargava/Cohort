1a40c6fdc6f3d2fcbf1f0175779eee913790e7a2
JatinBhargava <63741896+JatinBhargava@users.noreply.github.com>
2021-12-16 19:55:57 +0530

    Time: 42 ms (17.02%), Space: 25.6 MB (19.82%) - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-      vector<int>set1;
-      vector<int>set2;
-      reverse(nums.begin(),nums.end());
-      int n = nums.size();   
+      int n = nums.size();
+      vector<int>numscopy(n);
+        for(int i=0;i<n;i++){
+            numscopy[i] = nums[i];
+        }
         
-        k = k%nums.size();
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-       
+        
+        for(int i=0;i<n;i++)
+        {
+            nums[(i+k)%n] = numscopy[i];
+        }
     }
 };
+
+/*
+ for (int i = 0; i < n; i++)
+            {
+                nums[(i + k)%n] = numsCopy[i];
+            }
+*/            
