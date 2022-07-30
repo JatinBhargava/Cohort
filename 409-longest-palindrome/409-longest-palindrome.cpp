8d423354dc05a8dbdbc5ab0924bdc94a667a0c84
Jatin Bhargava <63741896+JatinBhargava@users.noreply.github.com>
2022-07-30 22:38:41 +0530

    Time: 17 ms (5.96%), Space: 8.1 MB (5.18%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -1,21 +1,22 @@
 class Solution {
-    public int longestPalindrome(String s) {
-        	if (s == null || s.length() == 0){
-            return 0;
+public:
+    int longestPalindrome(string s) {
+        int n = s.size();
+        int count = 0;
+        set<char>st;
+        for(int i=0;i<n;i++)
+        {
+            if(st.count(s[i]))
+            {
+                st.erase(s[i]);
+                count++;
+            }
+            else
+            {
+                st.insert(s[i]);
+            }
         }
-		HashSet<Character> hs = new HashSet<Character>();
-		int count = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (hs.contains(s.charAt(i))) {
-				hs.remove(s.charAt(i));
-				count++;
-			} else {
-				hs.add(s.charAt(i));
-			}
-		}
-		if (!hs.isEmpty()){
-            return count * 2 + 1;
-        }
-		return count * 2;
-    }
-}
+        
+        return st.empty()? count*2 : count*2 + 1;
+     }
+};
