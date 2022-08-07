f55b55d8071c80a244fb7bb8b5dea20d4be54377
Jatin Bhargava <63741896+JatinBhargava@users.noreply.github.com>
2022-08-08 01:16:21 +0530

    Time: 137 ms (56.40%), Space: 45.7 MB (20.46%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
     
-    int dp[10010];
+    //int dp[10010];
     
-    int func(vector<int>& coins, int amount) {
-       if(amount==0)
-           return 0;
-        if(dp[amount]!=-1) return dp[amount];
+    int func(int ind,vector<int>& coins, int amount,vector<vector<int>>&dp) {
         
-        int ans = INT_MAX;
-        for(auto coin:coins)
+        if(ind==0)
         {
-            if(amount-coin>=0)
-            {
-                ans = min(ans+0LL,func(coins,amount-coin)+1LL);
-            }
+            if(amount%coins[0]==0)
+                return amount/coins[0];
+            
+            return 1e9;
         }
-        return dp[amount] = ans;
+        
+        if(dp[ind][amount]!=-1)
+            return dp[ind][amount];
+        
+        int not_take = 0 + func(ind-1,coins,amount,dp);
+        int take = INT_MAX;
+        if(amount>=coins[ind])
+        {
+            take = 1 + func(ind,coins,amount - coins[ind],dp);
+        }
+        
+        return dp[ind][amount]  = min(not_take,take);
+        
+//        if(amount==0)
+//            return 0;
+//         if(dp[amount]!=-1) return dp[amount];
+        
+//         int ans = INT_MAX;
+//         for(auto coin:coins)
+//         {
+//             if(amount-coin>=0)
+//             {
+//                 ans = min(ans+0LL,func(coins,amount-coin)+1LL);
+//             }
+//         }
+//         return dp[amount] = ans;
     }
                           
       int coinChange(vector<int>& coins, int amount){
-          memset(dp,-1,sizeof(dp));
-          int ans = func(coins,amount);
-          return ans==INT_MAX? -1:ans;
+          int n = size(coins);
+          vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+          //memset(dp,-1,sizeof(dp));
+          int ans =  func(n-1,coins,amount,dp);
+          if(ans>=1e9)
+              return -1;
+          
+          return ans;
       }                    
 };
