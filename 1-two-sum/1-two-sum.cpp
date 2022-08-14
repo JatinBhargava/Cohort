085dac16dbc4c1450a51acd7dc946d0ccfffc87c
Jatin Bhargava <63741896+JatinBhargava@users.noreply.github.com>
2022-08-14 21:58:04 +0530

    Time: 24 ms (54.69%), Space: 11.1 MB (24.23%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,6 +2,49 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
+        vector<int>ans;
+        map<int,int>mpp;
+        for(int i=0;i<nums.size();i++){
+            if(mpp.count(target-nums[i]))
+                return {i,mpp[target-nums[i]]};
+            else
+                mpp[nums[i]] = i;
+        }
+        
+        return {-1,-1};
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        
         int n = nums.size();
         
         unordered_map<int,int>mpp;
@@ -12,5 +55,4 @@ public:
              mpp[a] = i;
         }
          return {-1,-1};
-    }
-};
+*/         
