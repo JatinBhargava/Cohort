940ae54fc225916a2a3fe4bb7c9dd503aac83358
Jatin Bhargava <63741896+JatinBhargava@users.noreply.github.com>
2022-10-19 02:23:46 +0530

    Time: 22 ms (28.91%), Space: 14 MB (92.27%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -4,16 +4,13 @@ public:
         int n = operations.size();
         
         int ans = 0;
-        for(int i=0;i<n;i++){
+        for(auto op:operations){
             //operations[0],[1],[2]
-            if(operations[i] == "--X")
-                --ans;
-            else if(operations[i] == "X++")
-                ans++;
-            else if(operations[i] == "++X")
-                ++ans;
-            else if(operations[i] == "X--")
-                ans--;
+          if(op[1] == '-')
+              ans--;
+          else
+              ans++;
+            
         }
         return ans;
     }
