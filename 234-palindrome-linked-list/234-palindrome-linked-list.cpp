11d9ded8ff1c8a71beb78440cf783eae5646fe12
Jatin Bhargava <63741896+JatinBhargava@users.noreply.github.com>
2023-06-24 01:08:07 +0530

    Time: 243 ms (75.56%), Space: 114 MB (89.56%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,7 +10,48 @@
  */
 class Solution {
 public:
-   bool isPalindrome(ListNode* head) {
+    bool isPalindrome(ListNode* head) {
+        
+         if(head == NULL || head->next == NULL){
+            return (head);
+        }
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        //slow-fast pointer concept
+        while(fast !=NULL and fast->next !=NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        //reverse a linked list concept
+        ListNode* pre = NULL;
+        while(slow !=NULL){
+            ListNode* temp = slow->next;
+            slow->next = pre;
+            pre = slow;
+            slow = temp;
+        }
+        
+        
+        ListNode* left = head;
+        ListNode* right = pre;
+        
+        while(right != NULL){
+            if(left->val != right->val)
+                return false;
+            
+            left = left->next;
+            right = right->next;
+        }
+        return true;
+    }
+};
+
+/*
+bool isPalindrome(ListNode* head) {
     if (head == NULL || head->next == NULL) {
         return true;
     }
@@ -45,5 +86,4 @@ public:
     }
     return true;
 }
-
-};
+*/
