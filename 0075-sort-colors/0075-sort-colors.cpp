85c79d5583ae9f639b1d63b2ac28862201e01d69
Jatin Bhargava <63741896+JatinBhargava@users.noreply.github.com>
2023-07-29 23:18:19 +0530

    Time: 0 ms (100%), Space: 8.1 MB (80.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,19 +5,16 @@ public:
         // sort 0,1,2
         
         //if 1 in mid -> left untouch
-        int lo = 0;
-        int mid = 0;
-        int hg = nums.size()-1;
+       int start = 0 , mid = 0, end = nums.size()-1;
         
-        while(mid <=hg)
-        {
+        while(mid <= end){
+            
             if(nums[mid] == 0)
-                swap(nums[lo++],nums[mid++]);
-            else if(nums[mid]==1)
+                swap(nums[start++],nums[mid++]);
+            else if(nums[mid] == 1)
                 mid++;
             else
-                swap(nums[mid],nums[hg--]);
+                swap(nums[mid],nums[end--]);
         }
-        
     }
 };
