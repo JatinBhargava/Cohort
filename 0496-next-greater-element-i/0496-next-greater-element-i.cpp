2cd257f67cc6404c5158602d41d9bcf1c15c3c3a
Jatin Bhargava <63741896+JatinBhargava@users.noreply.github.com>
2023-08-02 22:58:07 +0530

    Time: 4 ms (75.44%), Space: 9 MB (13.92%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        for(int i=0;i<nums1.size();i++){
-            bool flag=false;
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j])flag=true;
-                if(flag && nums1[i]<nums2[j]){
-                    ans.push_back(nums2[j]);flag=false;break;
+        
+        int n = nums2.size();
+
+        map<int,int>map;
+        map[nums2[n-1]] = -1;
+        vector<int>ans;
+
+        for(int i=0;i<n-1;i++){
+            int maxi = nums2[i];
+            for(int j=i+1;j<n;j++){
+                if(nums2[j] > maxi){
+                    maxi = nums2[j];
+                    break;
                 }
             }
-            if(flag)ans.push_back(-1);
-            
+            map[nums2[i]] = maxi;
+            if(map[nums2[i]] == nums2[i])
+                map[nums2[i]] = -1;
+        }
+
+        for(auto x:nums1)
+        {
+            if(map.find(x) != map.end())
+                ans.push_back(map[x]);
         }
         return ans;
     }
